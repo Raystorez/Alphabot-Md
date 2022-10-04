b505878947f709d15dd51d0b65de016354cb9039
Raystorez <113277838+Raystorez@users.noreply.github.com>
2022-10-04 14:58:25 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "RayBotz" //namabot kalian
-global.ownername= "RayXJoann" //nama kalian
+global.botname = "JoannBotz-Md" //namabot kalian
+global.ownername= "Joann Villain" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "raystore9375@gmaik.com" //bebas
+global.email = "joanstore654@gmaik.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['089673761414'] //premium user
-global.owner = ["+6289673761414"] //ganti agar fitur owner bisa di gunakan
+global.premium = ['6285729933146','6289673761414'] //premium user
+global.owner = ["6289673761414","6285729933146"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = "+6289673761414" // nomor wa kalian
-global.ownernomerr = "+6289673761414" //nmr wa kalian
+global.ownernomerr = "+6285729933146" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© RayBotz' //sticker wm ubah
-global.author = 'Di Buat Oleh RayBotz' //sticker wm ganti nama kalian
+global.packname = '© JoanBotz' //sticker wm ubah
+global.author = 'Di Buat Oleh JoannBotz' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
